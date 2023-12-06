c732ce69caf9aafa51c3df3d3c144d48badde19c
jackahl <kahl@kitconcept.com>
2023-12-06 15:49:13 +0100

    release-it configuration

---
+++ release-it.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+module.exports = {
+  npm: {
+    publish: true,
+  },
+
+  hooks: {
+    'after:bump':
+      'pipx run towncrier build --draft --yes --version ${version} > .changelog.draft && pipx run towncrier build --yes --version ${version}',
+    'after:release': 'rm .changelog.draft',
+  },
+  git: {
+    changelog: 'pipx run towncrier build --draft --yes --version 0.0.0',
+    requireUpstream: false,
+    requireCleanWorkingDir: false,
+    commitMessage: 'Release ${version}',
+    tagName: '${version}',
+    tagAnnotation: 'Release ${version}',
+  },
+  github: {
+    release: true,
+    releaseName: '${version}',
+    releaseNotes: 'cat .changelog.draft',
+  },
+};
